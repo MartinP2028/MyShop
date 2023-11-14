691fab2bc93f695d3e1df0ec3465e965b06d2b07
MartinP2028 <martin.pinaud@epitech.digital>
2023-11-14 14:21:41 +0100

    [upd] my folder

---
+++ my-shop-api/vue-project/src/stores/counter.js
@@ -0,0 +1,6 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useCounterStore = defineStore('counter', () => {
+})
---
+++ my-shop-api/vue-project/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>Je suis sur la page Home</h1>
+    <h2>{{ message }}</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World!",
+    };
+  },
+};
+</script>
+
+<style></style>
