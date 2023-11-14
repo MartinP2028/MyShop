20dc1a9c686588138e137501c4ac68d1959f4069
otinel2 <146081028+otinel2@users.noreply.github.com>
2023-11-14 11:27:58 +0100

    add otinel folder for starting

---
+++ my-shop-api/my-shop-api/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <h1>je suis sur la page Home</h1>
+        <h2>{{ message }}</h2>
+    </section>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            message: "Hello world"
+        }
+    }
+}
+</script>
+
+
+<style>
+</style>
