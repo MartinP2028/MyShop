9069c09deff0c0301086f8a9bb7372e2854cf1c3
otinel2 <146081028+otinel2@users.noreply.github.com>
2023-11-15 17:30:32 +0100

    Login

--- my-shop-api/vue-project/src/router/index.js
+++ my-shop-api/vue-project/src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Products from '../views/Products.vue'
-
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,6 +16,16 @@ const router = createRouter({
       name: "products",
       component: Products,
     },
+      {
+        path: '/Login',
+        name: 'Login',
+        component: Login,
+      },
+      {
+        path: '/Register',
+        name: 'Register',
+        component: Register,
+      },
   ],
 });
 
---
+++ my-shop-api/vue-project/src/views/Register.vue
@@ -0,0 +1,97 @@
+<template>
+<div class="register-container">
+    <h1>S'inscrire</h1>
+    <p class="register_subtile"> j'ai deja un compte,<RouterLink to="/Login"> me connecter </RouterLink></p>
+    <form class="register-form" @submit.prevent="register">
+      <label for="username">Nom d'utilisateur:</label>
+      <input v-model="username" type="text" id="username" required>
+
+      <label for="password">Mot de passe:</label>
+      <input v-model="password" type="password" id="password" required>
+
+      <label for="confirmPassword">Confirmer le mot de passe:</label>
+      <input v-model="confirmPassword" type="password" id="confirmPassword" required>
+
+      <button type="submit">S'inscrire</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      confirmPassword: '',
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        if (this.password !== this.confirmPassword) {
+          console.error('Les mots de passe ne correspondent pas.');
+          return;
+        }
+
+        const response = await axios.post('URL_DE_VOTRE_API/register', {
+          username: this.username,
+          password: this.password,
+        });
+
+        console.log('Inscription réussie', response.data);
+      } catch (error) {
+        console.error('Erreur d\'inscription', error);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.register-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: white;
+  width: 100vw;
+}
+
+.register-form {
+  width: 100%;
+  max-width: 400px;
+  padding: 20px;
+  border: 1px solid #a4a0a0;
+  background-color: #d1cfcf;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-top: 16px; 
+}
+label {
+  display: block;
+  margin-bottom: 8px;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 16px;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #0a0e12;
+  color: #fdfcfc;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+h1 {
+    color: black;
+}
+</style>
